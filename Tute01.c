0fa90923b4a2ae1d08fdc369b35c296d6388efbe
Nilusha Wijerathne <>
2022-02-09 06:30:50 +0000

    not completed

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,19 @@
 
 int main() {
   
+  int x,y;
+  float avg;
+
+  printf("Enter the mark for the 1subject :");
+  scanf("%d", &x);
+
+  printf("Enter the mark for the 2subject :");
+  scanf("%d", &y);
+
+  avg = (x+y)/2;
+
+  printf("Average : %.2f", avg);
+
   return 0;
 }
 
--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,10 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
+
+  int x
+
+  
   
   return 0;
 }
